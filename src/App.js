326a7f2de4a51d4a8a487487d9e896bcf43e8e15
Peeke Kuepers <pkuepers@kaliber.net>
2022-08-10 21:11:18 +0200

    Use pointer events, improve precision

--- src/App.js
+++ src/App.js
@@ -12,15 +12,17 @@ export default function App() {
   );
 }
 
-Selection.THRESHOLD = 20;
+Selection.MOUSE_THRESHOLD = 20;
+Selection.TOUCH_THRESHOLD = 50;
 
 function Selection() {
-  const ref = React.useRef(null);
+  const [node, setNode] = React.useState(null);
   const selectionRef = React.useRef(null);
   const frameRef = React.useRef(null);
-  const [containerWidth, containerHeight] = useSize(ref);
   const cropZoneRef = React.useRef(null);
-  const [activeEdges, setActiveEdges] = React.useState([]);
+  const stateRef = React.useRef({ dragging: false })
+  const [containerWidth, containerHeight] = useSize(node);
+  const [dragState, setDragState] = React.useState({ edges: [], dx: 0, dy: 0 });
 
   if (containerWidth && containerHeight && cropZoneRef.current === null) {
     cropZoneRef.current = {
@@ -31,14 +33,28 @@ function Selection() {
     };
   }
 
+  React.useEffect(
+    () => {
+      if (!node) return
+      node.addEventListener('touchmove', handleTouchMove)
+      node.addEventListener('pointerdown', handleDragStart)
+      node.addEventListener('pointermove', handleDrag, { passive: true })
+      node.addEventListener('pointerup', handleDragEnd)
+      node.addEventListener('pointercancel', handleDragEnd)
+
+      return () => {
+        node.removeEventListener('touchmove', handleTouchMove)
+        node.removeEventListener('pointerdown', handleDragStart)
+        node.removeEventListener('pointermove', handleDrag, { passive: true })
+        node.removeEventListener('pointerup', handleDragEnd)
+        node.removeEventListener('pointercancel', handleDragEnd)
+      }
+    },
+    [node, dragState]
+  )
+
   return (
-    <div
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-      onTouchCancel={handleTouchEnd}
-      ref={ref}
-    >
+    <div ref={setNode}>
       <div
         className={styles.selection}
         ref={selectionRef}
@@ -53,47 +69,71 @@ function Selection() {
     </div>
   );
 
-  function handleTouchStart(e) {
-    const { clientX: x, clientY: y } = e.touches[0];
-    setActiveEdges(getEdges({ x, y }));
+  function handleDragStart(e) {
+    e.preventDefault();
+    stateRef.current.dragging = true
+    const { clientX: x, clientY: y } = e
+    const threshold = e.pointerType === 'mouse' ? Selection.MOUSE_THRESHOLD : Selection.TOUCH_THRESHOLD
+    
+    setDragState(getEdges({ x, y, threshold }));
   }
 
   function handleTouchMove(e) {
-    e.preventDefault();
-    const { clientX: x, clientY: y } = e.touches[0];
-    if (activeEdges.includes("left")) {
+    if (!stateRef.current.dragging) return
+    e.preventDefault()
+  }
+
+  function handleDrag(e) {
+    if (!stateRef.current.dragging) return
+    
+    const x = e.clientX - dragState.dx
+    const y = e.clientY - dragState.dy
+    const threshold = e.pointerType === 'mouse' ? Selection.MOUSE_THRESHOLD : Selection.TOUCH_THRESHOLD
+
+    if (dragState.edges.includes("left")) {
       cropZoneRef.current.left = x;
-      cropZoneRef.current.right = Math.max(cropZoneRef.current.right, x + Selection.THRESHOLD);
-    } else if (activeEdges.includes("right")) {
+      cropZoneRef.current.right = Math.max(cropZoneRef.current.right, x + threshold);
+    } else if (dragState.edges.includes("right")) {
       cropZoneRef.current.right = x;
-      cropZoneRef.current.left = Math.min(cropZoneRef.current.left, x - Selection.THRESHOLD);
+      cropZoneRef.current.left = Math.min(cropZoneRef.current.left, x - threshold);
     }
 
-    if (activeEdges.includes("top")) {
+    if (dragState.edges.includes("top")) {
       cropZoneRef.current.top = y;
-      cropZoneRef.current.bottom = Math.max(cropZoneRef.current.bottom, y + Selection.THRESHOLD);
-    } else if (activeEdges.includes("bottom")) {
+      cropZoneRef.current.bottom = Math.max(cropZoneRef.current.bottom, y + threshold);
+    } else if (dragState.edges.includes("bottom")) {
       cropZoneRef.current.bottom = y;
-      cropZoneRef.current.top = Math.min(cropZoneRef.current.top, y - Selection.THRESHOLD);
+      cropZoneRef.current.top = Math.min(cropZoneRef.current.top, y - threshold);
     }
 
     updateSelection();
   }
 
-  function handleTouchEnd() {}
+  function handleDragEnd() {
+    stateRef.current.dragging = false
+  }
 
-  function getEdges({ x, y }) {
+  function getEdges({ x, y, threshold }) {
     const edges = [];
-    const dl = Math.abs(x - cropZoneRef.current.left);
-    const dr = Math.abs(x - cropZoneRef.current.right);
-    const dt = Math.abs(y - cropZoneRef.current.top);
-    const db = Math.abs(y - cropZoneRef.current.bottom);
-    if (dl <= dr && dl < Selection.THRESHOLD) edges.push("left");
-    else if (dr <= dl && dr < Selection.THRESHOLD) edges.push("right");
-    if (dt <= db && dt < Selection.THRESHOLD) edges.push("top");
-    else if (db <= dt && db < Selection.THRESHOLD) edges.push("bottom");
+    const dl = x - cropZoneRef.current.left;
+    const dr = x - cropZoneRef.current.right;
+    const dt = y - cropZoneRef.current.top;
+    const db = y - cropZoneRef.current.bottom;
+    const adl = Math.abs(dl);
+    const adr = Math.abs(dr);
+    const adt = Math.abs(dt);
+    const adb = Math.abs(db);
 
-    return edges;
+    if (adl <= adr && adl < threshold) edges.push("left");
+    else if (adr <= adl && adr < threshold) edges.push("right");
+    if (adt <= adb && adt < threshold) edges.push("top");
+    else if (adb <= adt && adb < threshold) edges.push("bottom");
+
+    return { 
+      dx: edges.includes('left') ? dl : edges.includes('right') ? dr : 0, 
+      dy: edges.includes('top') ? dt : edges.includes('bottom') ? db : 0, 
+      edges 
+    };
   }
 
   function updateSelection() {
